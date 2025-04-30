9bad5496f433ea669e704cd837dd556802b49320
KUYATE UMESH SHIVAJI <202401080042@mitaoe.ac.in>
2025-04-30 15:20:35 +0000

    switch to post

--- src/components/CodeExplainer.tsx
+++ src/components/CodeExplainer.tsx
@@ -27,7 +27,7 @@ const CodeExplainer = () => {
     setIsLoading(true);
     try {
       const response = await fetch('https://code-generator-x3de.onrender.com', {
-        method: 'GET',
+        method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
